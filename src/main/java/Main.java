3ca89681d8c797908a9515ebad76c5e98bbc246e
bc.kim <bc.kim@navercorp.com>
2024-02-16 01:08:48 +0900

    feat: Step 2
    - 기본 형태 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,20 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// You can use print statements as follows for debugging, they'll be visible when running tests.
 		System.out.println("Logs from your program will appear here!");
 
-		Socket clientSocket;
-		OutputStream outputStream;
-		try (var serverSocket = new ServerSocket(CommonConstant.REDIS_PORT)) {
-			serverSocket.setReuseAddress(true);
-			clientSocket = serverSocket.accept();
-
-			while ((outputStream = clientSocket.getOutputStream()) != null) {
-				sendResponse(outputStream, CommonConstant.REDIS_PONG_MESSAGE);
+		var serverSocket = new ServerSocket(CommonConstant.REDIS_PORT);
+		serverSocket.setReuseAddress(true);
+		while (true) {
+			try (var clientSocket = serverSocket.accept()) {
+				sendResponse(clientSocket.getOutputStream(), CommonConstant.REDIS_PONG_MESSAGE);
+			} catch (IOException e) {
+				System.out.printf("IOException: %s%n", e.getMessage());
 			}
-		} catch (IOException e) {
-			System.out.printf("IOException: %s%n", e.getMessage());
 		}
 	}
 
