d85dba154bf2ce15a5f6cd423d42b3f416bceaeb
VSFe <klm03025@gmail.com>
2024-02-21 02:31:46 +0900

    feat: RDB-step 4
    - 이후 Step 수행을 위한 Expire 관련 메서드 수정

--- src/main/java/redis/RedisExecuteException.java
+++ src/main/java/redis/RedisExecuteException.java
@@ -1,14 +1,7 @@
 package redis;
 
 public class RedisExecuteException extends RuntimeException {
-	public RedisExecuteException() {
-	}
-
 	public RedisExecuteException(String message) {
 		super(message);
-	}
-
-	public RedisExecuteException(String message, Throwable cause) {
-		super(message, cause);
 	}
 }
